60ebe06fb6ce9834b33cea45d780728109014555
divyam <divyam.prakash@eximietas.com>
2024-01-08 10:47:41 +0530

    feat:Add list and Style

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,23 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+      <h2>List with Different Bullets</h2>
+      <ul class="custom-list">
+        <li class="circle">Item 1</li>
+        <li class="square">Item 2</li>
+        <li class="star">Item 4</li>
+        <li class="custom">Item 5</li>
+      </ul>
     </div>
-  );
+    );
+
 }
 
 export default App;
